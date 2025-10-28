3922e91984dc744d181bdb40eceb29e25301429d
Juan Cotrina <jc99cj9@gmail.com>
2025-10-28 16:11:03 -0500

    conexion a la DB

---
+++ src/main/java/com/ELShovi/ElShoviBarGrillApplication.java
@@ -0,0 +1,13 @@
+package com.ELShovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElShoviBarGrillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElShoviBarGrillApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ELShovi/ElShoviBarGrillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ELShovi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElShoviBarGrillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
