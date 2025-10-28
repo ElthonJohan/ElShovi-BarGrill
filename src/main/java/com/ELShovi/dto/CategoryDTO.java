fad6be245cf0ae51ec9b53bf7e9247ff01030160
Elthon <N00338501@upn.pe>
2025-10-28 18:11:44 -0500

    Creaciones de dto

--- src/main/java/com/ELShovi/dto/CategoryDTO.java
+++ src/main/java/com/ELShovi/dto/CategoryDTO.java
@@ -1,4 +1,17 @@
 package com.ELShovi.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.antlr.v4.runtime.misc.NotNull;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class CategoryDTO {
+    private Integer id;
+    @NotNull
+    private String name;
+    @NotNull
+    private String description;
 }
