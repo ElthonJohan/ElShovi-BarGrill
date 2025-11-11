3d101c2932cb6bfd180a9565be71b7d483ebdb4b
Elthon <135673749+ElthonJohan@users.noreply.github.com>
2025-11-11 00:06:14 -0500

    Se agregó método para buscar por email

--- src/main/java/com/ELShovi/repository/IUserRepository.java
+++ src/main/java/com/ELShovi/repository/IUserRepository.java
@@ -1,6 +1,12 @@
 package com.ELShovi.repository;
 
 import com.ELShovi.model.User;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface IUserRepository extends IGenericRepository<User, Integer> {
+    Optional<User> findByEmail( String email);
+
 }
